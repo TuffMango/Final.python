b41d607d8c0c81fc84488f1b8b51688d1c20a312
TuffMango <therealgeorege@gmail.com>
2025-10-28 22:25:41 +0000

    added ultimates

--- Final.py
+++ Final.py
@@ -9,7 +9,7 @@ names = ["Akira", "Rin", "Ulqiorra", "Sora", "Ichi", "Haruto", "Aiko", "Renji", 
 melee_attacks = ["Jab", "Kick", "Dropkick", "Headbutt"]
 weak_magic_attacks = ["Cursed punch", "quick slice", "Piercing blood", "Dismantle", "Mass switch"]
 strong_magic_attacks = ["Cleave", "Cero Oscuras", "Detroit", "One Million Slashes", "120%", "Black Flash", "Getsuga Tensho"]
-Ultimate_moves = ["FINAL FLASH", "ILL USE MY ALL", "1000000%", "FINAL GETSUGA TENSHO"]
+ultimate_moves = ["FINAL FLASH", "ILL USE MY ALL", "1000000%", "FINAL GETSUGA TENSHO"]
 
 for i in range(user_input):     #takes the number of npcs the player wants and runs it through this command
     random_name =(random.choice(names))  #give one of x npcs a name from the list
@@ -22,16 +22,19 @@ for i in range(user_input):     #takes the number of npcs the player wants and r
     melee_move_two = (random.choice(melee_attacks))
     weak_magic = (random.choice(weak_magic_attacks))
     strong_magic = (random.choice(strong_magic_attacks))
-    if mana >=90:
-        ultimate = (random.choice(Ultimate_moves))
-
-
+    ultimate = (random.choice(ultimate_moves))
+    
 
     time.sleep(1.1)
-    print(f"-------{random_name}'s stats-------\n Name: {random_name} \n health: {health} \n defense: {defense} \n attack power: {attack_power} \n agility: {agility} \nMOVES\tDAMAGE\n{melee_move_one}\tplaceholder\n{melee_move_two}\tplaceholder\n{weak_magic_attacks}\tplaceholder\n{strong_magic}\tplaceholder\n\t--ULTIMATE MOVE--\n\t{ultimate}")
+    print(f"-------{random_name}'s stats-------\n Name: {random_name} \n health: {health} \n defense: {defense} \n attack power: {attack_power} \n agility: {agility} \n mana: {mana} \n----MOVES---- \t------DAMAGE----\n{melee_move_one}\tplaceholder\n{melee_move_two}\tplaceholder\n{weak_magic}\tplaceholder\n{strong_magic}\tplaceholder\n")
+    if mana >= 90:
+        print(f"--ULTIMATE MOVE--\n{ultimate}")
 
 
 if user_input == 2:
     fight_question = input("Do you want these characters to fight? ")
 else:
     print(" ")
+
+if fight_question == "yes" or "Yes":
+    
