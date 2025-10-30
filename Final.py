1fc56ed2df539b7d815485af19dc4d71f27f25f8
TuffMango <therealgeorege@gmail.com>
2025-10-30 16:02:30 +0000

    started attacks

--- Final.py
+++ Final.py
@@ -11,6 +11,12 @@ weak_magic_attacks = ["Cursed punch", "quick slice", "Piercing blood", "Dismantl
 strong_magic_attacks = ["Cleave", "Cero Oscuras", "Detroit", "One Million Slashes", "120%", "Black Flash", "Getsuga Tensho"]
 ultimate_moves = ["FINAL FLASH", "ILL USE MY ALL", "1000000%", "FINAL GETSUGA TENSHO"]
 
+
+
+
+
+
+
 for i in range(user_input):
     random_name =(random.choice(names))
     health = random.randint (1 , 100)
@@ -23,17 +29,72 @@ for i in range(user_input):
     weak_magic = (random.choice(weak_magic_attacks))
     strong_magic = (random.choice(strong_magic_attacks))
     ultimate = (random.choice(ultimate_moves))
+    
+
+    #DAMAGE
+    #WEAK MAGIC
+    cursed_punch_damage = 0
+    quick_slice_damage = 0
+    piercing_blood_damage = 0
+    dismantle_damage = 0
+    mass_switch_damage = 0
+    
+    if weak_magic == "Cursed punch":
+      cursed_punch_damage = 3 * {attack_power}
+    if weak_magic == "quick slice":
+      quick_slice_damage = 3 * .5 * {agility}
+    if weak_magic == "Piercing blood":
+      piercing_blood_damage = 5 * {attack_power}
+    
+    #Strong magic damage
+    cleave_damage = 0
+    cero_oscuras_damage = 0
+    detroit_damage =  0
+    one_million_slashes_damage = 0
+    one_twenty_damage = 0
+    black_flash_damage = 0
+    getsuga_tensho_damage = 0
+    if strong_magic == "Cleave":
+      cleave_damage = 8 *{attack_power}
+    if strong_magic == "Cero Oscuras":
+      cero_oscuras_damage = 6 * {attack_power}
+    if strong_magic == "Detroit":
+      detroit_damage = 6 * {attack_power}
+    if strong_magic == "One Million Slashes":
+      one_million_slashes_damage = 6 * {attack_power}
+    if strong_magic == "120%":
+      one_twenty_damage = .5 * {agility}
+    if strong_magic == "Black Flash":
+      black_flash_damage = 7 * {attack_power}
+    if strong_magic == "Getsuga Tensho":
+      getsuga_tensho_damage = 8 * {attack_power}
+  
+
+    #melee damage
+    jab_damage = 0
+    kick_damage = 0
+    dropkick_damage = 0
+    
+
+
     time.sleep(1.1)
-    print(f"-------{random_name}'s stats-------\n Name: {random_name} \n health: {health} \n defense: {defense} \n attack power: {attack_power} \n agility: {agility} \n mana: {mana} \n----MOVES---- \t------DAMAGE----\n{melee_move_one}\tplaceholder\n{melee_move_two}\tplaceholder\n{weak_magic}\tplaceholder\n{strong_magic}\tplaceholder\n")
+    print(f"-------{random_name}'s stats-------\n Name: {random_name} \n health: {health} \n defense: {defense} \n attack power: {attack_power} \n agility: {agility} \n mana: {mana} \n----MOVES---- \t------DAMAGE----\n{melee_move_one}\tN/A \n{melee_move_two}\tN/A\n{weak_magic}\tN/A\n{strong_magic}\tN/A")
+    
+
+
+
+
     if mana >= 90:
       print(f"--ULTIMATE MOVE--\n{ultimate}")
 
 
-fight_question = ("Do you want these characters to fight?(YOU WILL PLAY AS THE SECOND CHARACTER) ")
+
+
 if user_input == 2:
- answer = input(fight_question)
-if answer == "yes" or "Yes":
-  print(f"-------{random_name}'s stats-------\n Name: {random_name} \n health: {health} \n defense: {defense} \n attack power: {attack_power} \n agility: {agility} \n mana: {mana} \n----MOVES---- \t------DAMAGE----\n{melee_move_one}\tplaceholder\n{melee_move_two}\tplaceholder\n{weak_magic}\tplaceholder\n{strong_magic}\tplaceholder\n")
+ answer = input("Do you want these characters to fight?(YOU WILL PLAY AS THE SECOND CHARACTER) Y/N: ")
+
+if answer == "Y" or "y" or "Yes" or "yes":
+  print(f"-------{random_name}'s stats-------\n Name: {random_name} \n health: {health} \n defense: {defense} \n attack power: {attack_power} \n agility: {agility} \n mana: {mana} \n----MOVES---- \t------DAMAGE----\n{melee_move_one}\tplaceholder\n{melee_move_two}\tplaceholder\n{weak_magic}\tplaceholder\n{strong_magic}\tplaceholder\n", print(f"--ULTIMATE MOVE--\n{ultimate}"))
 else:
   print("okay!")
 
